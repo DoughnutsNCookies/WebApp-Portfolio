608fd1bd49109f097b43d2fb8192579b4945f028
Sean Chuah <chuahtseyung2002@gmail.com>
2023-07-07 18:23:03 +0800

    Background Timeline Bar can move

--- portfolio/src/pages/components/BackgroundTimeline.tsx
+++ portfolio/src/pages/components/BackgroundTimeline.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useRef } from "react";
+
 export const BackgroundTimeline = () => {
+  const backgroundRef = useRef<HTMLDivElement>(null);
+  const lineRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = (e: WheelEvent) => {
+      const element = backgroundRef.current;
+      const line = lineRef.current;
+      if (element && line) {
+        const { top, bottom } = element.getBoundingClientRect();
+        const ratio = Math.max(Math.min(top / (bottom - top - window.innerHeight), 0), -1);
+        line.style.transform = `translateX(${ratio * 100}%)`;
+      }
+    };
+    window.addEventListener("wheel", handleScroll);
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
+
   return (
-    <section id="background">
-      <div className="h-screen">Background</div>
+    <section id="background" ref={backgroundRef}>
+      <div
+        className="h-screen items-center justify-center"
+        style={{
+          height: "2100vh",
+        }}
+      >
+        <div
+          ref={lineRef}
+          className={`sticky top-[25vh] mt-[25vh] mb-[75vh] ml-[50vw] left-0 duration-300 border-2 w-[2100px]`}
+        />
+      </div>
     </section>
   );
 };
--- portfolio/src/pages/components/Projects.tsx
+++ portfolio/src/pages/components/Projects.tsx
@@ -1,6 +1,7 @@
 export const Projects = () => {
   return (
     <section id="projects">
+      <hr />
       <div className="h-screen">Projects</div>
     </section>
   );
